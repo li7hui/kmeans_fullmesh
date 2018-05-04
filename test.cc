b711330733cf86b32aba367fe68375c60a3c8fdb
li7hui <li7hui@gmail.com>
2018-05-03 23:23:04 -0400

    remove unesessary

--- test.cc
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(int argc, char **argv){
-	FILE *file = fopen(argv[2],"r");
-	fseek(file,0,SEEK_END);
-	int size = ftell(file);
-	printf("%e",size);
-	printf("%d",size);
-	printf("%d",size);
-}//int
--- test.cpp
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(int argc, char ** argv){
-	FILE *file = fopen(argv[1],"r");
-	fseek(file,0,SEEK_END);
-}
